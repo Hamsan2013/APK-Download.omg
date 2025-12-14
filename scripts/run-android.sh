1d244701cb058ab9c12e5acfb9eaddfde6d5c535
Hamsan2013 <hamsandivya@gmail.com>
2025-12-14 11:28:48 +0530

    Create run-android.sh

---
+++ scripts/run-android.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "=============================="
+echo "Android Launcher for Raspberry Pi"
+echo "Device check started..."
+echo "=============================="
+
+uname -a
+echo ""
+echo "NOTE:"
+echo "This script will later boot Android"
+echo "using an official Android image."
+echo ""
+echo "STATUS: Script executed successfully."
